c25a63bd5b953165ad6034beb704205c9e5124bb
Sangwon Choi <sangwon.choi@bhaptics.com>
2021-03-02 00:26:47 +0900

    Fix minor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React  from 'react';
+import * as ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import MainApp from './MainApp';
 
 
 function App() {
   return (
-    <div className={'mx-auto max-w-screen-xl h-screen bg-gray-100'}>
-      <div className={'text-2xl mx-4 py-2'}>
-        Convert Tool
+    <BrowserRouter>
+      <div className={'mx-auto max-w-screen-xl h-screen bg-gray-100'}>
+        <div className={'text-2xl mx-4 py-2'}>
+          Convert Tool
+        </div>
+
+        <MainApp/>
+
       </div>
-
-      <MainApp/>
-
-    </div>
+    </BrowserRouter>
   );
 }
 
